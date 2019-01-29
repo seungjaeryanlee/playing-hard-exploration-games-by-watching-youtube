014a2092688c56fefb9d759a18c4e94016a45616
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-01-29 12:53:16 +0900

    Implement get_checkpoints

--- train_agent.py
+++ train_agent.py
@@ -1,9 +1,32 @@
+import numpy as np
 import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 from commons import load_models
+from datasets import VideoDataset
 from networks import CMC, TDC
 from wrappers import make_env
 
+
+def get_checkpoint_loader():
+    filename = "./data/6zXXZvVvTFs"
+    trim = (960, 1403)
+    crop = (35, 50, 445, 300)
+    dataset = VideoDataset(filename, trim, crop, frame_rate=15)
+    loader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers=1)
+
+    return loader
+
+def get_checkpoints(tdc, loader):
+    embed_batches = []
+    for _, batch in enumerate(loader):
+        stack_batch, _ = batch
+        embed_batch = tdc(stack_batch.to(device))
+        embed_batch = F.normalize(embed_batch).cpu().detach().numpy()
+        embed_batches.append(embed_batch)
+
+    return embed_batches
 
 def train_agent(device):
     # Load embedded network
@@ -11,8 +34,10 @@ def train_agent(device):
     cmc = CMC().to(device)
     load_models(tdc, cmc)
 
-    # TODO Create checkpoints
-    checkpoints = []
+    # Create checkpoints
+    loader = get_checkpoint_loader()
+    checkpoints = get_checkpoints(tdc, loader)
+    print(checkpoints)
 
     # Create environment
     env = make_env((tdc, cmc), checkpoints)
